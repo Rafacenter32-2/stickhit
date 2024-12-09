a8051247fac8e542dced88ac6a514abe12eac76c
Rafael martinez <martinezvasconcelos64rafa@gmail.com>
2024-12-08 23:05:44 -0300

    refinement!

--- src/main.js
+++ src/main.js
@@ -2,3 +2,4 @@ import { k } from "./engine.js"
 import "./assets.js"
 import * as globalobjs from "./globalobj.js"
 import "./scenes/mainmenu/logo.js"
+go("logo")
--- src/scenes/mainmenu/logo.js
+++ src/scenes/mainmenu/logo.js
@@ -2,14 +2,15 @@
 import { k } from "../../engine.js"
 import "../../assets.js"
 import * as globalobjs from "../../globalobj.js"
-
-const logo = globalobjs.stick({
-    color:null,
-    position:center(),
-    islogo:true,
-    scale:0.3,
-    isrightstick:false
-})
-onUpdate(()=>{
-    k.debug.log(logo.stick.click)
+k.scene("logo",() => {
+    const logo = globalobjs.stick({
+        color:null,
+        position:center(),
+        islogo:true,
+        scale:0.3,
+        isrightstick:false
+    })
+    function beep() {
+        
+    }
 })
