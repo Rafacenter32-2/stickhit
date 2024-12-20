c48f6e92fea97f74f47339997af05c14c8790c83
Rafael <martinezvasconcelos64rafa@gmail.com>
2024-12-20 17:59:37 -0300

    added letterboxing also yea temp boilerplate code

--- src/engine.js
+++ src/engine.js
@@ -2,5 +2,17 @@
 import kaplay from "kaplay";
 import "kaplay/global";
  
-export const k = kaplay()
+export const k = kaplay({
+    buttons: {
+        left:{
+            gamepad:["lshoulder","ltrigger","west"]
+        },
+        right:{
+            gamepad:["rshoulder","rtrigger","south"]
+        }
+    },
+    letterbox:true,
+    width:1366,
+    height:768
+})
 
